e78c67ab8da4b704e408ba706747515b622a1531
Albera0 <yangxiaolong724@gmail.com>
2026-01-12 12:09:54 +0100

    地址簿业务代码开发

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -2,6 +2,7 @@ package com.sky.mapper;
 
 import com.sky.dto.ShoppingCartDTO;
 import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -32,4 +33,25 @@ public interface ShoppingCartMapper {
     @Insert("insert into shopping_cart(name, image, user_id, dish_id, setmeal_id, dish_flavor, amount, create_time, number) " +
             " values (#{name},#{image},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{amount},#{createTime},#{number})")
     void insert(ShoppingCart shoppingCart);
+
+    /**
+     * 清空购物车
+     * @param usertId
+     */
+    @Delete("delete from shopping_cart where user_id = #{userId}")
+    void deleteByUserId(Long usertId);
+
+    /**
+     * 根据id删除购物车数据
+     * @param id
+     */
+    @Delete("delete from shopping_cart where id = #{id}")
+    void deleteById(Long id);
+
+    /**
+     * 根据id修改购物车菜品份数
+     * @param shoppingCart
+     */
+    @Select("update shopping_cart set number = #{number} where id = #{id}")
+    void updateNumberById(ShoppingCart shoppingCart);
 }
